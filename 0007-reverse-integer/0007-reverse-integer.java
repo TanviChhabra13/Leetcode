f4ac0abca4ac7f25c94dde23ada0865d55022f48
Tanvi Chhabra <116876509+TanviChhabra13@users.noreply.github.com>
2025-03-27 23:04:37 +0530

    Time: 1 ms (83.91%), Space: 41.1 MB (33.55%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,29 +1,19 @@
 class Solution {
     public int reverse(int x) {
-        long i,digit,sum=0;
-        if(x<0)
-        {
-            x=Math.abs(x);
-            while(x>0)
-            {
-                digit=x%10;
-                x=x/10;
-                sum=sum*10+digit;
-            }
-            sum=sum*-1;
+        long finalNum = 0;
+        while(x!=0){
+            int lastDig = x%10;
+            finalNum += lastDig;
+            finalNum = finalNum*10;
+            x= x/10;
         }
-        else if(x>0)
-        {
-            while(x>0)
-            {
-                digit=x%10;
-                x=x/10;
-                sum=sum*10+digit;
-            }
+        finalNum = finalNum/10;
+        if(finalNum > Integer.MAX_VALUE || finalNum<Integer.MIN_VALUE){
+            return 0;
         }
-        if(sum<Integer.MIN_VALUE || sum >Integer.MAX_VALUE)
-        return 0;
-
-        return (int)sum;
+        if(x<0){
+            return (int)(-1*finalNum);
+        }
+        return (int)finalNum;
     }
 }
