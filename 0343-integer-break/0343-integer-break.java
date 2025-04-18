a325bffabb042f0c985a9190d2c37e845c8e906d
Tanvi Chhabra <116876509+TanviChhabra13@users.noreply.github.com>
2025-04-18 13:29:43 +0530

    Time: 0 ms (100%), Space: 40.4 MB (67.85%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int integerBreak(int n) {
+        if(n==2)
+        {
+            return 1;
+        }
+        if(n==3)
+        {
+            return 2;
+        }
+        if(n%3==0)
+        {
+            return (int)Math.pow(3,n/3);
+        }
+        if(n%3==1)
+        {
+            return (int)Math.pow(3,(n-4)/3) *4;
+        }
+        if(n%3==2)
+        {
+            return (int)Math.pow(3,(n-2)/3)*2;
+        }
+        return 0;
+    }
+}
